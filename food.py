e12c89d82acb573a1d934255605eb078f2076de4
Utsav0706 <utsavjivani.07@gmail.com>
2026-03-10 14:18:12 +0000

    Change in goto

--- food.py
+++ food.py
@@ -2,18 +2,19 @@ import turtle
 import random
 
 class Food(turtle.Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
+        self.penup()
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.color("Yellow")
-        self.penup()
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
         randomX = random.randint(-575, 575)
         randomY = random.randint(-300, 300)
-        self.goto(0,0)
+        self.goto(randomX,randomY)
 
 
