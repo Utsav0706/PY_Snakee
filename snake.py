c25dc262be484f2b9cc79265d7a1991dd57ab1a0
Utsav0706 <utsavjivani.07@gmail.com>
2026-03-10 14:00:53 +0000

    It is what it is

--- snake.py
+++ snake.py
@@ -2,12 +2,17 @@ import turtle
 
 STARTING_POSITION = [(0,0) ,(-20,0), (-40,0)]
 MOVE_SPEED = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.snakes = []
         self.create_snake()
+        self.head = self.snakes[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -24,4 +29,21 @@ class Snake:
 
             self.snakes[snake_num].goto(newX, newY)
 
-        self.snakes[0].forward(MOVE_SPEED)
+        self.head.forward(MOVE_SPEED)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
