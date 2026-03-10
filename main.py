6bf588a2783c6d81615a036859fe48b5734d023f
Utsav0706 <utsavjivani.07@gmail.com>
2026-03-10 13:10:43 +0000

    Build a snake

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ screen.bgcolor("black")
 screen.title("My Snake 😁")
 
 
+snake1 = turtle.Turtle("square")
+snake1.color("white")
+
+snake2 = turtle.Turtle("square")
+snake2.color("white")
+snake2.goto(-20,0)
+
+snake3 = turtle.Turtle("square")
+snake3.color("white")
+snake3.goto(-40,0)
+
 screen.exitonclick()
